e8e44306ca2aac236d4064cd76c332319a341273
rahulmisra2000 <rahulmisra2000@gmail.com>
2018-05-29 00:17:35 -0400

    aaaa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm  } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  email: string;
+
+constructor() {   }
+
+
+ public onSubmit(f: NgForm) {
+      console.log('OnSubmit: ');
+      console.log(f);
+
+ }
 }
+
